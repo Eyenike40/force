e5ca4c641491bb056f0480cd17e1463774c0de64
Eyenike Nelson <eyenikenelson@gmail.com>
2023-11-09 17:43:04 +0100

    The function to compute the force of a body was done
    The program compute the force of a body. Also, a get object mass input from the
    user was a function done seperately.

---
+++ main.py
@@ -0,0 +1,48 @@
+def main():
+    """
+    The main program
+    """
+    mass = get_object_mass()
+    force = compute_force(mass)
+    print(f"The force of the body = {force:,.2f}N")
+
+
+def get_object_mass():
+    """
+    This function get the mass of the object
+
+    Paramters
+    ---------
+    None
+
+    Return
+    ------
+    It return the mass of the object
+    """
+
+    while True:
+        try:
+            return float(input("Enter the mass of the body (Kg) : "))
+        except ValueError:
+            print("Enter a valid number : ")
+
+
+def compute_force(mass: float):
+    """
+    This function compute the force of a body
+
+    Parameter
+    ---------
+    The mass of a body
+
+    Return
+    ------
+    The force of the body
+    """
+    acceleration = 9.81
+
+    return acceleration * mass
+
+
+if "__main__" == __name__:
+    main()
